26f0b0bb6e157a6de139b805a432855a2b7a2a36
Davide Bettio <davide@uninstall.it>
2024-01-05 02:57:23 +0100

    Support different kind of displays
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- lib/main.ex
+++ lib/main.ex
@@ -1,22 +1,45 @@
 defmodule Main do
+  _sharp_display_opts = [
+    width: 400,
+    height: 240,
+    compat: "sharp_memory_lcd",
+    cs: 5,
+    en: 27
+  ]
+
+  _ili_display_opts = [
+    width: 320,
+    height: 240,
+    compat: "ili934x",
+    reset: 18,
+    cs: 22,
+    dc: 21,
+    backlight: 5
+  ]
+
+  sdl_display_opts = [
+    width: 320,
+    height: 240
+  ]
+
+  @display_driver sdl_display_opts
+  @spi_display_driver? false
+
   def start() do
     :erlang.display("Hello.")
 
-    spi_opts = %{
-      bus_config: %{mosi: 25, sclk: 26, peripheral: "spi2"},
-      device_config: %{}
-    }
+    display_opts =
+      if @spi_display_driver? do
+        spi_opts = %{
+          bus_config: %{sclk: 19, mosi: 23, miso: 25, peripheral: "spi2"},
+          # bus_config: %{mosi: 25, sclk: 26, peripheral: "spi2"}, sharp
+          device_config: %{}
+        }
 
-    spi = :spi.open(spi_opts)
-
-    display_opts = [
-      width: 400,
-      height: 240,
-      compat: "sharp_memory_lcd",
-      cs: 5,
-      en: 27,
-      spi_host: spi
-    ]
+        [{:spi_host, :spi.open(spi_opts)} | @display_driver]
+      else
+        @display_driver
+      end
 
     case :erlang.open_port({:spawn, "display"}, display_opts) do
       display when is_port(display) ->
